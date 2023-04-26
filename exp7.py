5b1b6011bd67b169db8a9e31f867de08f2f3f5ce
Harsh Vardhan <jobbykarn@gmail.com>
2023-04-26 06:47:36 +0530

    2

--- exp7.py
+++ exp7.py
@@ -5,12 +5,15 @@ P4 = False
 P5 = False
 
 rule1 = P1 and P2
-rule2 = P1 and P2 and P3 and not P4
+rule2 = P1 and P2 and P3 and not P4 
 rule3 = P1 and P2 and P3 and not P4 and P5
 
 if (rule3):
     print("The customer can rent SUV")
 elif(rule2):
     print("Customer can rent Sedan")
-elif(rule2):
-    print("The Customer is not elligible")
+elif(rule1):
+    print("any car")
+else:
+    print("ineligible")
+
